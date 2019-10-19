ab3557ecff95a6f52374a0c89dd1ab2f61a1e9a4
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2019-10-19 16:08:14 +0530

    Initial version

---
+++ README.md
@@ -0,0 +1,3 @@
+## Start server
+
+Run `node server.js` to start the node server. Make sure to update the config settings in `.env` file
---
+++ app.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const postRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(
+    "mongodb+srv://" +
+      process.env.MONGO_ATLAS_USER +
+      ":" +
+      process.env.MONGO_ATLAS_PW +
+      "@" +
+      process.env.MONGO_ATLAS_SERVER +
+      "/" +
+      process.env.MONGO_ATLAS_DB +
+      "?retryWrites=true",
+    { useNewUrlParser: true,
+      useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use('/images', express.static(path.join('backend/images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  next();
+});
+
+app.use('/api/posts', postRoutes);
+app.use('/api/user', userRoutes);
+
+module.exports = app;
